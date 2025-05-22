518fa1b2e7c59dea2adac01246c21924f72cc13b
KareimT <kareimtalat@gmail.com>
2025-05-22 17:48:25 +0300

    Make the Entity for Room

--- app/src/main/java/com/kareimt/anwarresala/data/local/CourseEntity.kt
+++ app/src/main/java/com/kareimt/anwarresala/data/local/CourseEntity.kt
@@ -1,2 +1,24 @@
 package com.kareimt.anwarresala.data.local
 
+
+@Entity(tableName = "courses")
+data class CourseEntity(
+    @PrimaryKey val id: Int,
+    val branch: String,
+    val imageResId: Int,
+    val category: String,
+    val title: String,
+    val type: String, // enum سيتم تعريفه // Use String or Int for enums in Room
+    val instructorName: String,
+    val instructorBio: String,
+    val instructorImageResId: Int,
+    val startDate: String,
+    val wGLink: String? = null, // Optional
+    val courseDetails: String? = null, // Optional
+    val totalLectures: Int,
+    val noOfLiteraturesFinished: Int,
+    val nextLecture: String? = null, // Optional
+    val organizerName: String? = null, // Optional
+    val organizerWhatsapp: String? = null // Optional
+    val process: Float
+)
